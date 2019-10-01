23378d915d460c841f45243c0a0f9f353523a72a
AlikVaniev <bartolomeo.rizzo@mail.ru>
2019-10-01 12:38:17 +0300

    alik work

--- n2/main.js
+++ n2/main.js
@@ -1,1 +1,31 @@
-// Your code here
+let canv = document.createElement('canvas');
+let ctx = canv.getContext('2d');
+
+canv.width = window.innerWidth;
+canv.height = window.innerWidth;
+
+document.body.appendChild(canv);
+
+let X;
+init();
+
+function init() {
+	X=100;
+	loop();
+
+}
+
+
+function loop() {
+	draw();
+
+	requestAnimationFrame(loop);
+}
+
+function draw() {
+
+ctx.clearRect(100, 100, window.innerWidth, window.innerHeight);
+							/*x,y,width,height*/
+ctx.strokeRect(canv.width / 2 - 100, canv.height / 2 - 100, 200, 200);
+
+}
--- n3/main.js
+++ n3/main.js
@@ -1,1 +1,36 @@
-// Your code here
+let canv = document.createElement('canvas');
+let ctx = canv.getContext('2d');
+
+canv.width = window.innerWidth;
+canv.height = window.innerWidth;
+
+document.body.appendChild(canv);
+
+let x;
+let y;
+init();
+
+function init() {
+	x=30;
+	y=30;
+	loop();
+
+}
+
+
+function loop() {
+	draw();
+
+	requestAnimationFrame(loop);
+}
+
+function draw() {
+
+ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+ctx.beginPath();
+ctx.fillStyle ='rgb(255,0,0)';						
+ctx.arc(x++, y++, 20, 0, Math.PI * 2);
+ctx.fill();
+ctx.closePath();
+
+}// Your code here
